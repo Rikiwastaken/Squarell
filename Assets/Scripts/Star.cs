c19c64c51f4926d3f77419a64a6b2e5dafa7f95d
arnaudkrafft <arnaud.krafft@telecomnancy.net>
2024-12-22 14:50:02 +0100

    added shooting stars

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEditor.Animations;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Star : MonoBehaviour
@@ -14,6 +15,11 @@ public class Star : MonoBehaviour
 
     public AnimatorController Animationtoshow;
     public Sprite SpriteToShow;
+
+    private int chancetobecomeashootingstar;
+    public int shootingstarmax;
+    public float xtowarp;
+    public float ytowarp;
 
     // Update is called once per frame
 
@@ -35,24 +41,52 @@ public class Star : MonoBehaviour
     }
     void FixedUpdate()
     {
-        GetComponent<Rigidbody2D>().velocity = mvtspeed;
-        GetComponent<Rigidbody2D>().transform.rotation = Quaternion.Euler(GetComponent<Rigidbody2D>().transform.rotation.eulerAngles + new Vector3(0f,0f,rotation));
-        if(transform.position.x>20)
+        Vector2 posparent = transform.parent.position;
+
+
+        if(transform.position.x-posparent.x>xtowarp)
         {
-            transform.position = new Vector2(-20, transform.position.y);
+            transform.position = new Vector2(-xtowarp + posparent.x, transform.position.y);
+            becomeshootingstar(true);
         }
-        if (transform.position.x < -20)
+        else if (transform.position.x - posparent.x < -xtowarp)
         {
-            transform.position = new Vector2(20, transform.position.y);
+            transform.position = new Vector2(xtowarp + posparent.x, transform.position.y);
+            becomeshootingstar(true);
         }
-        if (transform.position.y > 12)
+        else if (transform.position.y - posparent.y > ytowarp)
         {
-            transform.position = new Vector2(transform.position.x, -12);
+            transform.position = new Vector2(transform.position.x, -ytowarp + posparent.y);
+            becomeshootingstar(true);
         }
-        if (transform.position.y < -12)
+        else if (transform.position.y - posparent.y < -ytowarp)
         {
-            transform.position = new Vector2(transform.position.x, 12);
+            transform.position = new Vector2(transform.position.x, ytowarp + posparent.y);
+            becomeshootingstar(true);
+        }
+        else
+        {
+            becomeshootingstar(false);
         }
         
+    }
+
+    void becomeshootingstar(bool move)
+    {
+        if(move)
+        {
+            chancetobecomeashootingstar = (int)Random.Range(0, shootingstarmax);
+        }
+        
+        if(chancetobecomeashootingstar==shootingstarmax-3)
+        {
+            GetComponent<Rigidbody2D>().velocity = mvtspeed*10;
+            GetComponent<Rigidbody2D>().transform.rotation = Quaternion.Euler(GetComponent<Rigidbody2D>().transform.rotation.eulerAngles + new Vector3(0f, 0f, rotation*35));
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = mvtspeed;
+            GetComponent<Rigidbody2D>().transform.rotation = Quaternion.Euler(GetComponent<Rigidbody2D>().transform.rotation.eulerAngles + new Vector3(0f, 0f, rotation));
+        }
     }
 }
--- Assets/Scripts/StarSpawner.cs
+++ Assets/Scripts/StarSpawner.cs
@@ -16,17 +16,21 @@ public class StarSpawner : MonoBehaviour
     public Vector2 mvtspeed;
     public float rotation;
     public float size;
+    public Vector2 screensize;
+    public int shootingstarmax;
+    private GameObject starholder;
 
     void Start()
     {
 
+        starholder = new GameObject("starholder");
         for(int i = 0; i < nbrang; i++)
         {
             for(int j = 0; j < starparrang; j++)
             {
                 float posx= (float)(i+1) / (float)nbrang;
                 float posy = (float)(j + 1) / (float)starparrang;
-                Vector2 position = new Vector2((posx * 40) - 20, (posy * 20) - 10) + new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f)) ;
+                Vector2 position = transform.position = new Vector2((posx * screensize.x) - screensize.x/2, (posy * screensize.y) - screensize.y/2) + new Vector2(Random.Range(-screensize.x/4, screensize.x / 4), Random.Range(-screensize.y / 4, screensize.y / 4)) ;
                 GameObject newstar = Instantiate(star, position, Quaternion.identity);
                 if (Animationtoshow != null)
                 {
@@ -38,9 +42,22 @@ public class StarSpawner : MonoBehaviour
                 }
                 newstar.GetComponent<Star>().mvtspeed = mvtspeed;
                 newstar.GetComponent<Star>().rotation = rotation;
+                newstar.GetComponent<Star>().xtowarp = (screensize.x/2.0f)+1.0f;
+                newstar.GetComponent<Star>().ytowarp = (screensize.y / 2.0f) + 1.0f;
+                newstar.GetComponent<Star>().shootingstarmax = shootingstarmax;
                 newstar.transform.localScale = Vector2.one*size;
-                newstar.transform.parent = transform;
+                newstar.transform.parent = starholder.transform;
+                
             }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameObject.Find("Main Camera"))
+        {
+            transform.position = new Vector3(GameObject.Find("Main Camera").transform.position.x, GameObject.Find("Main Camera").transform.position.y, transform.position.z);
+            starholder.transform.position = new Vector3(GameObject.Find("Main Camera").transform.position.x, GameObject.Find("Main Camera").transform.position.y, starholder.transform.position.z);
         }
     }
 
